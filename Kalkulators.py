2dbf73bba37baaaa4d20386cd339d68ce5f3abcd
MarissZZZ <marisszuss@gmail.com>
2022-03-04 14:21:51 +0200

    Gatavs

--- Kalkulators.py
+++ Kalkulators.py
@@ -7,8 +7,12 @@ import string
 from tkinter import*
 from turtle import color # importē bibiliotēku
 from unittest.util import strclass
+import tkinter as tk
+
 mansLogs=Tk()
 mansLogs.title('Kalkulators')
+
+
 
 #================================================================================================================
 def btnClick(number):
@@ -97,28 +101,36 @@ def notirit():
     return 0
 
 #================================================================================================================
+def punkts():
+    if e.get()==".":
+        pass
+    else:
+        e.insert(END,'.')
+
+#================================================================================================================
 e=Entry(mansLogs,width=15,bd=20,font=("Arial Black",20))
 #================================================================================================================
-btn0=Button(mansLogs,text='0',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(0))
-btn1=Button(mansLogs,text='1',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(1))
-btn2=Button(mansLogs,text='2',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(2))
-btn3=Button(mansLogs,text='3',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(3))
-btn4=Button(mansLogs,text='4',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(4))
-btn5=Button(mansLogs,text='5',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(5))
-btn6=Button(mansLogs,text='6',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(6))
-btn7=Button(mansLogs,text='7',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(7))
-btn8=Button(mansLogs,text='8',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(8))
-btn9=Button(mansLogs,text='9',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='green', command=lambda:btnClick(9))
-btnAdd=Button(mansLogs,text='+',padx='40',pady='20', bd=10, font=('Arial Black',20), command=lambda:btnCommand("+"))
-btnSub=Button(mansLogs,text='-',padx='43',pady='20', bd=10, font=('Arial Black',20), command=lambda:btnCommand("-"))
-btnReiz=Button(mansLogs,text='*',padx='40',pady='20', bd=10, font=('Arial Black',20), command=lambda:btnCommand("*"))
-btnDal=Button(mansLogs,text='/',padx='43',pady='20', bd=10, font=('Arial Black',20), command=lambda:btnCommand("/"))
-btnC=Button(mansLogs,text='C',padx='39',pady='20', bd=10, font=('Arial Black',20), fg='red', bg='light green', command=notirit)
-btnVien=Button(mansLogs,text='=',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='light green', command=vienads)
-btnKom=Button(mansLogs,text='log',padx='27',pady='20', bd=10, font=('Arial Black',20), command=logoritms)
-btnPlusMin=Button(mansLogs,text='+/-',padx='31',pady='20', bd=10, font=('Arial Black',20), command=min)
-btnIek=Button(mansLogs,text='x²',padx='35',pady='20', bd=10, font=('Arial Black',20), command=kvad)
-btnProc=Button(mansLogs,text='√',padx='41',pady='20', bd=10, font=('Arial Black',20), command=sq_rt)
+btn0=Button(mansLogs,text='0',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(0))
+btn1=Button(mansLogs,text='1',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(1))
+btn2=Button(mansLogs,text='2',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(2))
+btn3=Button(mansLogs,text='3',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(3))
+btn4=Button(mansLogs,text='4',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(4))
+btn5=Button(mansLogs,text='5',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(5))
+btn6=Button(mansLogs,text='6',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(6))
+btn7=Button(mansLogs,text='7',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(7))
+btn8=Button(mansLogs,text='8',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(8))
+btn9=Button(mansLogs,text='9',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='white', command=lambda:btnClick(9))
+btnAdd=Button(mansLogs,text='+',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=lambda:btnCommand("+"))
+btnSub=Button(mansLogs,text='-',padx='43',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=lambda:btnCommand("-"))
+btnReiz=Button(mansLogs,text='*',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=lambda:btnCommand("*"))
+btnDal=Button(mansLogs,text='/',padx='43',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=lambda:btnCommand("/"))
+btnC=Button(mansLogs,text='C',padx='39',pady='20', bd=10, font=('Arial Black',20), fg='red', bg='cyan', command=notirit)
+btnVien=Button(mansLogs,text='=',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=vienads)
+btnKom=Button(mansLogs,text='log',padx='25.5',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=logoritms)
+btnPlusMin=Button(mansLogs,text='+/-',padx='31',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=min)
+btnIek=Button(mansLogs,text='x²',padx='34.5',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=kvad)
+btnProc=Button(mansLogs,text='√',padx='41.4',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=sq_rt)
+btnPunkts=Button(mansLogs,text='.',padx='40',pady='20', bd=10, font=('Arial Black',20), bg='cyan', command=punkts)
 
 #================================================================================================================
 e.grid(row=0,column=0,columnspan=4) # ievieto režģī
